2e7135ce16c21db4e61a74983c28201cee49684a
Paalar <paaledward@gmail.com>
2020-10-26 15:56:16 +0100

    Correctly set prune value

--- prune_dataset.py
+++ prune_dataset.py
@@ -10,10 +10,10 @@ def read_csv() -> DataFrame:
 def save_csv(filename, dataset):
     dataset.to_csv(filename, index=False)
 
-def prune_value(row, value, new_value):
+def prune_value(row, predicate, new_value):
     new_row = row.copy()
     for index, value in enumerate(new_row):
-        if (value == -8):
+        if (value == predicate):
             new_row[index] = new_value
     return new_row
 
@@ -22,17 +22,23 @@ def prune_dataset(dataset: DataFrame) -> DataFrame:
     columns = pruned_dataset.columns
     columns = columns.drop('RiskPerformance')
     for index, value in enumerate(pruned_dataset.iterrows()):
-        label, content = value
+        if index % 1000 == 0:
+            save_csv("heloc_dataset_pruned.csv", pruned_dataset)
+        _, content = value
+        print(f"Row #{index}")
         # Rows with -9 are all invalid data
-        if ((content[columns] == -9).all()):
+        row = content
+        if ((row[columns] == -9).all()):
             pruned_dataset = pruned_dataset.drop(index)
-        # Row, columns with value -8 are invalid
-        if ((content[columns] == -8).any()):
-            row = prune_value(content[columns], -8, 0)
+            continue
+        # Row, columns with value -7, -8, -9 are also invalid
+        if ((row[columns] == -9).any()):
+            row = prune_value(row[columns], -9, 0)
+        if ((row[columns] == -8).any()):
+            row = prune_value(row[columns], -8, 0)
         if ((row[columns] == -7).any()):
-            row = prune_value(content[columns], -7, 0)
-        pruned_dataset[index] = row
-
+            row = prune_value(row[columns], -7, 0)
+        pruned_dataset.loc[index, columns] = row
     return pruned_dataset
 
 def main():
