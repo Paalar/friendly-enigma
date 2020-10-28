641cbf17db25567bc8e0c0d24ab4baec6dbe77cb
Johannes Kvamme <22146437+FluidSense@users.noreply.github.com>
2020-10-28 15:54:21 +0100

    Split data out to module

---
+++ helocDataModule.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from pytorch_lightning import LightningDataModule
+from helocDataset import HELOCDataset
+from torch.utils.data import DataLoader
+
+
+class HelocDataModule(LightningDataModule):
+
+    def __init__(self, validation_size: int = 2, workers: int = 8, batch_size: int = 64):
+        super().__init__()
+        self.validation_size = validation_size
+        self.workers = workers
+        self.batch_size = batch_size
+
+    def prepare_data(self):
+        CSV_FILE = "./heloc_dataset_v1.csv"
+        self.data = pd.read_csv(CSV_FILE)
+        self.row_length = self.data.shape[1] - 1 # Remove predictor
+
+    def setup(self, step):
+        # step is either 'fit' or 'test', can be used to read only necessary data.
+        # Read and split data
+        dataset = self.data
+        dataset_length = len(dataset.values)
+        self.training_split = dataset[:dataset_length//self.validation_size]
+        confirmation_split = dataset[dataset_length//self.validation_size:]
+        self.test_split = confirmation_split[:len(confirmation_split)//self.validation_size]
+        self.validate_split = confirmation_split[len(confirmation_split)//self.validation_size:]
+
+
+    def train_dataloader(self):
+        HELOC_train = HELOCDataset(self.training_split)
+        return DataLoader(HELOC_train, num_workers=self.workers, batch_size=self.batch_size)
+        
+
+    def val_dataloader(self):
+        HELOC_validate = HELOCDataset(self.validate_split)
+        return DataLoader(HELOC_validate, num_workers=self.workers, batch_size=self.batch_size)
+        
+
+    def test_dataloader(self):
+        HELOC_test = HELOCDataset(self.test_split)
+        return DataLoader(HELOC_test, num_workers=self.workers, batch_size=self.batch_size)
--- main.py
+++ main.py
@@ -4,44 +4,18 @@ import pytorch_lightning as pl
 import os
 
 # Subpackage
-from torch.utils.data import DataLoader
 from pandas import DataFrame
 from typing import Tuple
 from pytorch_lightning.loggers import CometLogger, TensorBoardLogger
 
 # Project imports
-from helocDataset import HELOCDataset
 from model import Net
 from multiTaskOutputWrapper import MultiTaskOutputWrapper
-
-VALIDATION_SIZE = 2
-CSV_FILE = "./heloc_dataset_v1.csv"
-
-def read_csv() -> DataFrame:
-    return pd.read_csv(CSV_FILE)
-
-def setup(dataset: DataFrame) -> Tuple[DataLoader, DataLoader]:
-    # Read and split data
-    dataset_length = len(dataset.values)
-
-    # Instantiate DataSets
-    training_split = dataset[:dataset_length//VALIDATION_SIZE]
-    HELOC_train = HELOCDataset(training_split)
-    train_loader = DataLoader(HELOC_train, num_workers=8, batch_size=64)
-
-    confirmation_split = dataset[dataset_length//VALIDATION_SIZE:]
-    test_split = confirmation_split[:len(confirmation_split)//VALIDATION_SIZE]
-    HELOC_test = HELOCDataset(test_split)
-    test_loader = DataLoader(HELOC_test, num_workers=8, batch_size=64)
-
-    validate_split = confirmation_split[len(confirmation_split)//VALIDATION_SIZE:]
-    HELOC_validate = HELOCDataset(validate_split)
-    validate_loader = DataLoader(HELOC_validate, num_workers=8, batch_size=64)
-    return train_loader, validate_loader, test_loader
+from helocDataModule import HelocDataModule
 
 def main():
-    heloc_dataset = read_csv()
-    train_loader, validate_loader, test_loader = setup(heloc_dataset)
+    data_module = HelocDataModule()
+    data_module.prepare_data()
 
     # Configure logging
     api_key = os.environ.get('COMET_API_KEY')
@@ -53,12 +27,12 @@ def main():
 
     # Instantiate model
     nodes_before_split = 64
-    input_length = len(train_loader.dataset[0][0])
+    input_length = data_module.row_length
     net = Net(input_length=input_length, output_length=nodes_before_split)
     model = MultiTaskOutputWrapper(model_core=net, input_length=nodes_before_split, output_length=(1,1))
     trainer = pl.Trainer(max_epochs=150, logger=logger)
-    trainer.fit(model, train_loader, validate_loader)
-    trainer.test(model,test_loader)
+    trainer.fit(model, data_module)
+    trainer.test(model, datamodule=data_module)
 
 if __name__ == "__main__":
     main()
