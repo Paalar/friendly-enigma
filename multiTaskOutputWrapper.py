4dd6b28b43872de0206100e418bae4e160190374
Johannes Kvamme <22146437+FluidSense@users.noreply.github.com>
2020-10-28 13:20:57 +0100

    Add loss per head

--- multiTaskOutputWrapper.py
+++ multiTaskOutputWrapper.py
@@ -21,12 +21,18 @@ class MultiTaskOutputWrapper(pl.LightningModule):
         # Hyperparameters
         self.learning_rate = 0.01
 
+        # https://towardsdatascience.com/multi-task-learning-with-pytorch-and-fastai-6d10dc7ce855
+        self.log_vars = nn.Parameter(torch.zeros((2)))
+
         # Pre-split model
         self.rest_of_model = model_core
 
         # Heads
         self.prediction_head = nn.Linear(input_length, output_length[0])
         self.explanation_head = nn.Linear(input_length, output_length[1])
+
+        # Loss functions per head
+        self.loss_functions = [F.mse_loss, F.binary_cross_entropy]
 
 
     def forward(self, data_input):
@@ -35,17 +41,19 @@ class MultiTaskOutputWrapper(pl.LightningModule):
         # Heads
         prediction = self.prediction_head(rest_output)
         prediction = torch.sigmoid(prediction)
-        #explanation = self.explanation_head(rest_output)
-        #explanation = F.softmax(explanation, dim=-1)
-        return prediction
+        explanation = self.explanation_head(rest_output)
+        explanation = torch.sigmoid(explanation)
+        return prediction, explanation
 
     def training_step(self, batch, _):
         values, correct_label = batch
-        prediction = self(values)
-        loss = F.mse_loss(prediction, correct_label)
-        self.log('Loss/train', loss)
+        prediction, explanation = self(values)
+        loss_prediction = self.calculate_loss(prediction, correct_label, 0)
+        loss_explanation = self.calculate_loss(explanation, correct_label, 1)
+        self.log('Loss/train-prediction', loss_prediction)
+        self.log('Loss/train-explanation', loss_explanation)
         self.metrics_update("train-step", prediction, correct_label)
-        return loss
+        return loss_prediction+loss_explanation
 
     def training_epoch_end(self, outs):
         # Log epoch metric
@@ -53,16 +61,19 @@ class MultiTaskOutputWrapper(pl.LightningModule):
 
     def validation_step(self, batch, _):
         values, correct_label = batch
-        prediction = self(values)
-        loss = F.mse_loss(prediction, correct_label)
-        self.log('Loss/validate', loss)
+        prediction, explanation = self(values)
+        loss_prediction = self.calculate_loss(prediction, correct_label, 0)
+        loss_explanation = self.calculate_loss(explanation, correct_label, 1)
+        self.log('Loss/validate', loss_prediction)
+        return loss_prediction+loss_explanation
 
     def test_step(self,batch, _):
         values, correct_label = batch
-        prediction = self(values)
-        loss = F.mse_loss(prediction, correct_label)
+        prediction, explanation = self(values)
+        loss_prediction = self.calculate_loss(prediction, correct_label, 0)
+        loss_explanation = self.calculate_loss(explanation, correct_label, 1)
         self.metrics_update("test-step", prediction, correct_label)
-        self.log('Loss/test', loss)
+        self.log('Loss/test', loss_prediction)
 
     def test_epoch_end(self, outs):
         self.metrics_compute("test-epoch")
@@ -82,3 +93,8 @@ class MultiTaskOutputWrapper(pl.LightningModule):
         self.log(f"Recall/{label}", self.recall(prediction, correct_label))
         self.log(f"Fbeta/{label}", self.fbeta(prediction, correct_label))
 
+    def calculate_loss(self, prediction, correct_label, head_number):
+        loss_function = self.loss_functions[head_number]
+        loss = loss_function(prediction, correct_label)
+        precision = torch.exp(-self.log_vars[head_number])
+        return precision * loss + self.log_vars[head_number]
