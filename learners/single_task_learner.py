410f4b03406039f1a7abb393d7ef7e7373cea602
Paalar <paaledward@gmail.com>
2020-11-04 13:52:25 +0100

    Rename core model

--- learners/single_task_learner.py
+++ learners/single_task_learner.py
@@ -1,10 +1,7 @@
 # Package imports
-import pandas as pd
 import pytorch_lightning as pl
 
 # Subpackage
-from pandas import DataFrame
-from typing import Tuple
 from datetime import datetime
 from pytorch_lightning.callbacks import ModelCheckpoint
 
@@ -44,31 +41,3 @@ def create_checkpoint_callback():
         dirpath=f"./checkpoints/{datetime.now().isoformat()}",
         filename="heloc-{epoch:02d}-{loss_validate:.2f}",
     )
-
-
-# def main():
-#     logger = dashboard.create_logger()
-#     data_module = HelocDataModule()
-#     data_module.prepare_data()
-
-#     # Instantiate model
-#     nodes_before_split = 64
-#     input_length = data_module.row_length
-
-#     net = Net(input_length=input_length, output_length=nodes_before_split)
-
-#     model = SingleTaskOutputWrapper(
-#         model_core=net,
-#         input_length=nodes_before_split,
-#         output_length=1,
-#     )
-
-#     trainer = pl.Trainer(
-#         max_epochs=13, logger=logger, checkpoint_callback=create_checkpoint_callback()
-#     )
-#     trainer.fit(model, data_module)
-#     trainer.test(model, datamodule=data_module)
-
-
-# if __name__ == "__main__":
-#     main()
