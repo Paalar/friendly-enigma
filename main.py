4fd7d8807035165a3a4354030e517d9d813e1836
Paalar <paaledward@gmail.com>
2020-11-04 13:39:41 +0100

    Fix small stuff

---
+++ main.py
@@ -0,0 +1,9 @@
+from single_task_learner import SingleTaskLearner
+from multi_task_learner import MultiTaskLearner
+
+def main():
+    learner = MultiTaskLearner()
+    learner.run()
+
+if __name__ == "__main__":
+    main()
