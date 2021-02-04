52f65bb9c2ceb9349951b2329dec83bc2903921b
Pål Larsen <paaledward@gmail.com>
2021-02-04 11:59:54 +0100

    Replace colon with dash so files wont crash on windows

--- runners/STL_runner.py
+++ runners/STL_runner.py
@@ -57,6 +57,6 @@ def create_checkpoint_callback(prefix):
     return ModelCheckpoint(
         monitor="loss_validate",
         save_top_k=3,
-        dirpath=f"./checkpoints/{prefix}-{datetime.now().strftime('%y-%m-%d-%H:%M:%S')}",
+        dirpath=f"./checkpoints/{prefix}-{datetime.now().strftime('%y-%m-%d-%H-%M-%S')}",
         filename="heloc-{epoch:02d}-{loss_validate:.2f}",
     )
