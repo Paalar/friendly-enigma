9ca6894a135832be5ba425acf01783217d677240
Paalar <paaledward@gmail.com>
2021-02-22 15:59:31 +0100

    Update how to split data for cchvae

--- data/cchvae_split.py
+++ data/cchvae_split.py
@@ -2,15 +2,16 @@ import pandas as pd
 import numpy as np
 
 heloc = "./heloc_dataset_v1_pruned.csv"
-
 df = pd.read_csv(heloc)
 records = df.to_numpy()
 
-heloc_x_c = {"col1": records[:,1], "col2": records[:,2], "col3": records[:,4]}
-heloc_x = {"col0": records[:,3]}
+types_column_names = ["type", "dim", "nclass"]
 
-for index in range(5, len(records[0])):
-    heloc_x[f"row{index}"] = records[:,index]
+locked_features = [1, 2, 4]
+free_features = [i for i in range(1, 24) if (i not in locked_features)]
+
+heloc_x_c = {index: records[:,value] for index, value in enumerate(locked_features)}
+heloc_x = {index: records[:,value] for index, value in enumerate(free_features)}
 
 heloc_y = records[:,0]
 heloc_y = [1 if value == "Good" else 0 for value in heloc_y]
@@ -19,6 +20,14 @@ def save(file, filename):
     df_file = pd.DataFrame(file).astype(float)
     df_file.to_csv(filename, header=False, index=False)
 
+heloc_types_c = [["count", 1,""] for i in range(len(heloc_x_c.keys()))]
+heloc_types = [["real", 1,""] for i in range(len(heloc_x.keys()))]
+
+df_types = pd.DataFrame(heloc_types, columns=types_column_names)
+df_types_c = pd.DataFrame(heloc_types_c, columns=types_column_names)
+
+df_types.to_csv("heloc_types.csv", index=False)
+df_types_c.to_csv("heloc_types_c.csv", index=False)
 save(heloc_x_c, "heloc_x_c.csv")
 save(heloc_x, "heloc_x.csv")
 save(heloc_y, "heloc_y.csv")
