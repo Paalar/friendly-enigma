726198f943747afae06ea76237ad6ec234c00125
Johannes Kvamme <22146437+FluidSense@users.noreply.github.com>
2021-03-05 11:11:41 +0100

    Bigger test than validate

--- data/cchvaeDataModule.py
+++ data/cchvaeDataModule.py
@@ -5,21 +5,28 @@ from data.helocDataModule import HelocDataModule
 from torch.utils.data import DataLoader
 from .cchvaeDataset import CchvaeDataset
 
+
 class CchvaeDataModule(HelocDataModule):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-    
+
     def prepare_data(self):
         super().prepare_data()
-        DELTA_COUNTERFACTUALS_CSV_FILE = "data/delta_counterfactuals.csv"
+        DELTA_COUNTERFACTUALS_CSV_FILE = "data/aggregated_delta_counterfactuals.csv"
         counterfactuals_data = pd.read_csv(DELTA_COUNTERFACTUALS_CSV_FILE, header=None)
-        squashed_counterfactuals = ["".join(str(row)) for row in counterfactuals_data.values[:,1:]]
+        squashed_counterfactuals = [
+            "".join(str(row)) for row in counterfactuals_data.values[:, 1:]
+        ]
         self.data.insert(0, "counterfactual delta", squashed_counterfactuals)
+        # self.data.to_csv("heloc_with_delta_counterfactuals.csv")
 
     def train_dataloader(self):
         HELOC_train = CchvaeDataset(self.training_split)
         return DataLoader(
-            HELOC_train, num_workers=self.workers, batch_size=self.batch_size
+            HELOC_train,
+            num_workers=self.workers,
+            batch_size=self.batch_size,
+            shuffle=True,
         )
 
     def val_dataloader(self):
--- data/helocDataModule.py
+++ data/helocDataModule.py
@@ -16,11 +16,15 @@ class HelocDataModule(LightningDataModule):
     ):
         super().__init__()
         self.validation_size = validation_size
-        self.workers = config["cpu_workers"] if type(config["cpu_workers"]) is int else workers
-        self.batch_size = config["batch_size"] if type(config["batch_size"]) is int else batch_size
+        self.workers = (
+            config["cpu_workers"] if type(config["cpu_workers"]) is int else workers
+        )
+        self.batch_size = (
+            config["batch_size"] if type(config["batch_size"]) is int else batch_size
+        )
 
     def prepare_data(self):
-        CSV_FILE = "data/heloc_dataset_v1_pruned.csv"
+        CSV_FILE = "data/aggregated_counterfactuals.csv"
         self.data = pd.read_csv(CSV_FILE)
         self.row_length = self.data.shape[1] - 1  # Remove predictor
         self.labels = self.data.columns[1:]
@@ -33,16 +37,19 @@ class HelocDataModule(LightningDataModule):
         self.training_split = dataset[: dataset_length // self.validation_size]
         confirmation_split = dataset[dataset_length // self.validation_size :]
         self.test_split = confirmation_split[
-            : len(confirmation_split) // self.validation_size
+            len(confirmation_split) // self.validation_size :
         ]
         self.validate_split = confirmation_split[
-            len(confirmation_split) // self.validation_size :
+            : len(confirmation_split) // self.validation_size
         ]
 
     def train_dataloader(self):
         HELOC_train = HELOCDataset(self.training_split)
         return DataLoader(
-            HELOC_train, num_workers=self.workers, batch_size=self.batch_size
+            HELOC_train,
+            num_workers=self.workers,
+            batch_size=self.batch_size,
+            shuffle=True,
         )
 
     def val_dataloader(self):
