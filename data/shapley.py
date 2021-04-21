75733182da52f2e74f518bbf51d3e390994d318d
Paalar <paaledward@gmail.com>
2021-04-21 12:10:43 +0200

    Make shapley values sort of work

--- data/shapley.py
+++ data/shapley.py
@@ -1,7 +1,9 @@
+from data.normalized_labels import Labels
 import pandas as pd
 import torch
+from tqdm import tqdm
 
-from captum.attr import ShapleyValues
+from captum.attr import ShapleyValueSampling
 from pandas.core.frame import DataFrame
 from models.singleTaskLearner import SingleTaskLearner
 
@@ -10,25 +12,43 @@ def split_targets_predictors(dataset: DataFrame):
     predictors = dataset.iloc[:,1:]
     return targets, predictors
 
-heloc_checkpoint = "cchvae/classifier-heloc.ckpt"
+def split_normalized(dataset):
+    df = DataFrame(dataset)
+    return split_targets_predictors(df)
+
+# heloc_checkpoint = "cchvae/classifier-heloc.ckpt"
+heloc_checkpoint = "checkpoints/stl-21-04-21-08-11-33/heloc-epoch=196-loss_validate=0.37-top-validate.ckpt"
 
 heloc = pd.read_csv("data/heloc/heloc_dataset_v1_pruned.csv", index_col=False)
+# print(heloc)
 heloc_c = pd.read_csv("data/heloc/counterfactuals.csv", header=None, index_col=False)
 amplified_heloc_c = pd.read_csv("data/heloc/augmented_counterfactuals.csv", index_col=False)
 
+heloc_t = heloc["RiskPerformance"].map(lambda target: 1 if target == "Good" else 0)
+heloc["RiskPerformance"] = heloc_t
+
 classifier = SingleTaskLearner.load_from_checkpoint(heloc_checkpoint)
-sv = ShapleyValues(classifier)
+sv = ShapleyValueSampling(classifier)
 
-targets, predictors = split_targets_predictors(heloc)
+heloc_shapley_values = []
+labels = Labels(heloc.to_numpy())
+targets, predictors = split_normalized(labels.labels)
+with tqdm(total=len(targets)) as progress_bar:
+    for target, predictor in zip(targets, predictors.values):
+        try:
+            target = int(target)
+            inp = torch.tensor(predictor, dtype=torch.float32).unsqueeze(0)
+            attributions = sv.attribute(inp, target=target)
+            heloc_shapley_values.append(attributions)
+        except:
+            print("target", target)
+            print("features", predictor)
+            print("input", inp)
+            print("feil")
+        finally:
+            progress_bar.update(1)
+    
+        
 
-input = torch.tensor(predictors.iloc[0].values, dtype=torch.float32)
-target = 1 if targets.iloc[0] == "Good" else 0
-# target = torch.tensor([target], dtype=torch.float32)
-# torch.tensor(["Good"])
-# input = torch.tensor(input)
-print(input)
-
-attributions, delta = sv.attribute(input, target=target)
-
-print(attributions)
-print(delta)
+df = DataFrame(heloc_shapley_values, index=None)
+df.to_csv("data/heloc/shapley_heloc_pruned.csv")
